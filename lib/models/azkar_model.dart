539ba54b356a05621cd93f88b0737accd006e996
ismail_ghanem <ismail.ghanem995@gmail.com>
2023-08-01 21:00:31 +0300

    create type adapter for azkar model

--- lib/models/azkar_model.dart
+++ lib/models/azkar_model.dart
@@ -1,5 +1,12 @@
-class AzkarModel {
+import 'package:hive/hive.dart';
+
+part 'azkar_model.g.dart';
+
+@HiveType(typeId: 0)
+class AzkarModel extends HiveObject {
+  @HiveField(0)
   final String zekr;
+  @HiveField(1)
   final int numOfZekr;
 
   AzkarModel({
