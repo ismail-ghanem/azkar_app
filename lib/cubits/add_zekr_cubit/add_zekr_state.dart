d42f7b8f2b37957a3ea55fc745b909750fd3dbd3
ismail_ghanem <ismail.ghanem995@gmail.com>
2023-08-09 20:07:18 +0300

    add zekr state

--- lib/cubits/add_zekr_cubit/add_zekr_state.dart
+++ lib/cubits/add_zekr_cubit/add_zekr_state.dart
@@ -4,3 +4,13 @@ part of 'add_zekr_cubit.dart';
 abstract class AddZekrState {}
 
 class AddZekrInitial extends AddZekrState {}
+
+class AddZekrLoading extends AddZekrState {}
+
+class AddZekrSuccess extends AddZekrState {}
+
+class AddZekrFailure extends AddZekrState {
+  final String errMessage;
+
+  AddZekrFailure(this.errMessage);
+}
