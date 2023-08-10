f2707145d7b0aab3a6b0b05f63d78c8faca01e9a
ismail_ghanem <ismail.ghanem995@gmail.com>
2023-08-10 20:31:23 +0300

    add 'add zekr cubit' to add button action

--- lib/widgets/add_button_action.dart
+++ lib/widgets/add_button_action.dart
@@ -1,16 +1,33 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+
+import '../cubits/add_zekr_cubit/add_zekr_cubit.dart';
 import '../widgets/custom_button.dart';
 import '../widgets/custom_text_field.dart';
 import 'package:flutter/material.dart';
 
 class AddButtonAction extends StatelessWidget {
   const AddButtonAction({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 16),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
       child: SingleChildScrollView(
-        child: AddNewForm(),
+        child: BlocConsumer<AddZekrCubit, AddZekrState>(
+          listener: (context, state) {
+            if (state is AddZekrFailure) {
+              print('failed ${state.errMessage}');
+            }
+            if (state is AddZekrSuccess) {
+              Navigator.pop(context);
+            }
+          },
+          builder: (context, state) {
+            return ModalProgressHUD(
+                inAsyncCall: state is AddZekrLoading ? true : false,
+                child: const AddNewForm());
+          },
+        ),
       ),
     );
   }
