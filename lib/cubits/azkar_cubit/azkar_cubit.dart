08c3642812c8e297b17d4c9e9bde4fe81530b678
ismail_ghanem <ismail.ghanem995@gmail.com>
2023-09-02 20:04:37 +0300

    create fetch azkar method

--- lib/cubits/azkar_cubit/azkar_cubit.dart
+++ lib/cubits/azkar_cubit/azkar_cubit.dart
@@ -1,9 +1,22 @@
 import 'package:azkar_app/models/azkar_model.dart';
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import '../../const.dart';
 
 part 'azkar_state.dart';
 
 class AzkarCubit extends Cubit<AzkarState> {
   AzkarCubit() : super(AzkarInitial());
+  fetchAzkar() async {
+    try {
+      var azkarBox = Hive.box<AzkarModel>(kAzkarBox);
+
+      emit(AzkarSuccess(
+        azkarBox.values.toList(),
+      ));
+    } catch (e) {
+      emit(AzkarFailure(e.toString()));
+    }
+  }
 }
--- lib/views/azkar_view.dart
+++ lib/views/azkar_view.dart
@@ -1,3 +1,6 @@
+import 'package:azkar_app/cubits/azkar_cubit/azkar_cubit.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
 import '../widgets/add_button_action.dart';
 import 'package:flutter/material.dart';
 import '../widgets/azkar_view_body.dart';
@@ -7,29 +10,32 @@ class AzkarView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          showModalBottomSheet(
-              isScrollControlled: true,
-              shape: const RoundedRectangleBorder(
-                borderRadius: BorderRadius.only(
-                  topRight: Radius.circular(32),
-                  topLeft: Radius.circular(32),
+    return BlocProvider(
+      create: (context) => AzkarCubit(),
+      child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            showModalBottomSheet(
+                isScrollControlled: true,
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(32),
+                    topLeft: Radius.circular(32),
+                  ),
                 ),
-              ),
-              context: context,
-              builder: (context) {
-                return const AddButtonAction();
-              });
-        },
-        backgroundColor: Colors.green,
-        child: const Icon(
-          Icons.add,
-          size: 36,
+                context: context,
+                builder: (context) {
+                  return const AddButtonAction();
+                });
+          },
+          backgroundColor: Colors.green,
+          child: const Icon(
+            Icons.add,
+            size: 36,
+          ),
         ),
+        body: const AzkarViewBody(),
       ),
-      body: const AzkarViewBody(),
     );
   }
 }
